61cc62ea5c7f3396d11d7d9f9a3c41d02935b20c
Eagan Martin <it-dev@scinnova.com.ph>
2019-11-19 12:51:57 +0800

    Revised structure

--- src/config/dependency-injector.js
+++ src/config/dependency-injector.js
@@ -1,6 +1,6 @@
 const DatabaseService = demand('frameworks/databases/mongodb/MongoDbService');
 const BackendService = demand('frameworks/backend/express/ExpressService');
-const LoggerService = demand('frameworks/loggers/Winston');
+const LoggerService = demand('frameworks/utils/Winston');
 
 module.exports = (() => {
     return {
